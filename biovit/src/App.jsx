5a0348f528b8862e7f16b88c7b3a6fb00ef1420b
Nikita <nikitamaryniuk@gmail.com>
2024-05-10 14:24:57 +0200

    footer done

--- biovit/src/App.jsx
+++ biovit/src/App.jsx
@@ -1,13 +1,17 @@
-import React from "react";
-import './App.css';
+import React from 'react'
+import './App.css'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Layout from './components/Layout/Layout'
 
 const App = () => {
-    return (
-        <div >
-            App
-        </div>
-    );
+	return (
+		<div>
+			<Header />
+			<Layout />
+			<Footer />
+		</div>
+	)
 }
 
-export default App;
-
+export default App
---
+++ biovit/src/components/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from './Hero/Hero'
+
+const Layout = () => {
+	return (
+		<div className='min-h-screen'>
+			<Hero />
+		</div>
+	)
+}
+
+export default Layout
